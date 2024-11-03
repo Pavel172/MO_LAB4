9314b26f876a91b12f62edeb0e3d5c4a05bdbff4
Павел Лустин <144838661+Pavel172@users.noreply.github.com>
2024-11-04 01:02:00 +0300

    Update Program.cs
    Завершение метода полного перебора

--- Program.cs
+++ Program.cs
@@ -28,6 +28,50 @@ namespace MO_LAB3
             Console.WriteLine("Метод полного перебора: ");
             List<List<double>> combinations = new List<List<double>>(); //список, в котором будут находиться все возможные комбинации 0 и 1 для переменных
             GenerateCombinations(vec_c.Count, new List<double>(), ref combinations); //ищем все возможные комбинации 0 и 1
+            for (int i = 0; i < combinations.Count; ++i) //проверяем каждую комбинацию на допустимость
+            {
+                sum = 0;
+                F = 0;
+                for (int j = 0; j < combinations[i].Count; ++j) 
+                {
+                    F += vec_c[j] * combinations[i][j]; //находим значение функции при данных иксах
+                    sum += vec_a[j] * combinations[i][j]; //находим сумму иксов для проверки ограничений
+                }
+                if (sum > b) continue; //проверяем ограничения
+                if (F_min > F) F_min = F; //находим минимальное значение функции среди решений
+                answer = new List<double>(combinations[i]);
+                answer.Insert(0, F);
+                answer_list.Add(answer); //добавляем ответ в список ответов
+            }
+            for(int i = 0; i < answer_list.Count; ++i) //выводим в консоль все допустимые решения и показываем их допустимость
+            {
+                if (flag == 1) answer_list[i][0] = (-1) * answer_list[i][0]; //если мы меняли стремление функции с max на min, то возращаем его назад
+                Console.WriteLine($"\n{i+1}-ое решение:");
+                if (answer_list[i][0] == F_min) Console.WriteLine("Это оптимальное решение, так как здесь функция минимальна");
+                for (int j = 0; j < answer_list[i].Count; ++j)
+                {
+                    if (j == 0) Console.Write($"F={answer_list[i][j]}  ");
+                    if (j != 0) Console.Write($"X{j}={answer_list[i][j]}  "); //печатаем все иксы и F
+                }
+                Console.Write("\nF = ");
+                F = 0;
+                for (int g = 0; g < vec_c.Count; ++g) //печатаем значение функции при данных переменных
+                {
+                    Console.Write($"{vec_c[g]} * {answer_list[i][g+1]}"); //g+1, так как в списке на 1-ом месте значение функции
+                    if (g != vec_c.Count - 1) Console.Write(" + ");
+                    F += answer_list[i][g+1] * vec_c[g];
+                }
+                Console.Write($" = {F}\n");
+                sum = 0;
+                for (int l = 0; l < answer_list[i].Count-1; ++l) //показываем правильность полученных переменных путем подстановки их значений в ограничение
+                {
+                    Console.Write($"{vec_a[l]} * {answer_list[i][l+1]}");
+                    sum += vec_a[l] * answer_list[i][l + 1];
+                    if (l != vec_a.Count - 1) Console.Write(" + ");
+                }
+                Console.Write($" <= {b} \t{sum} <= {b}");
+                if (sum <= b) Console.Write(" - верно\n");
+            }
         } 
 
         static void GenerateCombinations(int n, List<double> current, ref List<List<double>> result) //рекурсивный метод, создающий все возможные комбинации 0 и 1 для переменных
